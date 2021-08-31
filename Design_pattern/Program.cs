2d3621b9d2908eb88b269d00d5b092aea2320373
vasanth <vasanthhari321@gmail.com>
2021-08-31 12:26:32 +0530

    Singleton Design Pattern

---
+++ Design_pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Design_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+    * Assuming Singleton is created from employee class
+    * we refer to the GetInstance property from the Singleton class
+    */
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+            /*
+             * Assuming Singleton is created from student class
+             * we refer to the GetInstance property from the Singleton class
+             */
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Design_pattern/Singleton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_pattern
+{
+     //* Sealed ensures the class being inherited and
+     //* object instantiation is restricted in the derived class
+     //*/
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+
+        /*
+         * Private property initilized with null
+         * ensures that only one instance of the object is created
+         * based on the null condition
+         */
+        private static Singleton instance = null;
+
+        /*
+         * public property is used to return only one instance of the class
+         * leveraging on the private property
+         */
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        /*
+         * Private constructor ensures that object is not
+         * instantiated other than with in the class itself
+         */
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        /*
+         * Public method which can be invoked through the singleton instance
+         */
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+    }
+}
